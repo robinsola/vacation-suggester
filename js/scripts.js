e9fe2dcd05f282c41177233f5ea447d9a82af4f0
Robin Sola <rsolarms@gmail.com>
2017-10-25 11:52:48 -0700

    add new variables for locations and set them to zero, add styling for text and button in css and add form and script for entering user name.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,80 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-      var comfort = parseInt($("select#comfort").val());
-      var meal = parseInt($("select#meal").val());
-      var activity = parseInt($("select#activity").val());
-      var book = parseInt($("select#book").val());
-      var philosophy = parseInt($("select#philosophy").val());
+      var nameInput = $("input#name").val();
 
-      var result = (comfort + meal + activity + book + philosophy);
+      var comfort = $("select#comfort").val();
+      var meal = $("select#meal").val();
+      var activity = $("select#activity").val();
+      var book = $("select#book").val();
+      var philosophy = $("select#philosophy").val();
 
-      if (result <= 45) {
+      var shire = 0;
+      var starship = 0;
+      var overlook = 0;
+      var bathhouse = 0;
+
+      if (comfort === "shire") {
+        shire += 1;
+      } else if (comfort === "starship") {
+        starship += 1;
+      } else if (comfort === "overlook") {
+        overlook += 1;
+      } else if (comfort === "bathhouse") {
+        bathhouse += 1;
+      }
+
+      if (meal === "shire") {
+        shire += 1;
+      } else if (meal === "starship") {
+        starship += 1;
+      } else if (meal === "overlook") {
+        overlook += 1;
+      } else if (meal === "bathhouse") {
+        bathhouse += 1;
+      }
+
+      if (activity === "shire") {
+        shire += 1;
+      } else if (activity === "starship") {
+        starship += 1;
+      } else if (activity === "overlook") {
+        overlook += 1;
+      } else if (activity === "bathhouse") {
+        bathhouse += 1;
+      }
+
+      if (book === "shire") {
+        shire += 1;
+      } else if (book === "starship") {
+        starship += 1;
+      } else if (book === "overlook") {
+        overlook += 1;
+      } else if (book === "bathhouse") {
+        bathhouse += 1;
+      }
+
+      if (philosophy === "shire") {
+        shire += 1;
+      } else if (philosophy === "starship") {
+        starship += 1;
+      } else if (philosophy === "overlook") {
+        overlook += 1;
+      } else if (philosophy === "bathhouse") {
+        bathhouse += 1;
+      }
+
+
+      if (shire >= 3) {
         $('#shire').fadeIn();
-      } else if (result >= 46 && result <= 75) {
+      } else if (starship >= 3) {
         $('#starship').fadeIn();
-      } else if (result >= 76 && result <= 125) {
+      } else if (overlook >= 3) {
         $('#overlook').fadeIn();
       } else {
         $('#bathhouse').fadeIn();
       }
+
+      $(".name").text(nameInput);
 
       $("#quiz").hide();
       $(".jumbotron").hide();
